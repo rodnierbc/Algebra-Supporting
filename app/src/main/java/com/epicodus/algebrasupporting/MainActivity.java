b049ecfb5cf21c1745df63a8cab0d5ca01fcc752
Rodnier Borrego <rodnierbc89@gmail.com>
2018-03-16 18:50:25 -0700

    Create MainOptionsArrayAdapter java class

--- app/src/main/java/com/epicodus/algebrasupporting/MainActivity.java
+++ app/src/main/java/com/epicodus/algebrasupporting/MainActivity.java
@@ -2,12 +2,36 @@ package com.epicodus.algebrasupporting;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.mainOptionsList) ListView mMainOptionsList;
+
+    private String[] mOptions = new String[] {"Evaluate","Solve","Plot"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        MainOptionsArrayAdapter adapter = new MainOptionsArrayAdapter(this, android.R.layout.simple_list_item_single_choice, mOptions ); //must match constructor!
+        mMainOptionsList.setAdapter(adapter);
+
+        mMainOptionsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String restaurant = ((TextView)view).getText().toString();
+
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/epicodus/algebrasupporting/MainOptionsArrayAdapter.java
@@ -0,0 +1,29 @@
+package com.epicodus.algebrasupporting;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by rodnier.borrego on 3/16/18.
+ */
+
+public class MainOptionsArrayAdapter extends ArrayAdapter{
+    private Context mContext;
+    private String[] mOptions;
+
+    public MainOptionsArrayAdapter(Context mContext, int resource, String[] mOptions) {
+        super(mContext, resource);
+        this.mContext = mContext;
+        this.mOptions = mOptions;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mOptions[position];
+    }
+    @Override
+    public int getCount() {
+        return mOptions.length;
+    }
+}
+
